79be82834985cc93c03ef25226d68c5a9aecc662
juliamarotti <julealmarotti@gmail.com>
2026-03-23 16:51:52 -0300

    outros testes

--- test_cantina.py
+++ test_cantina.py
@@ -7,3 +7,17 @@ def test_calcular_total():
 def test_calcular_total_zero():
     assert calcular_total(0, 10) == 0
 
+def test_calcular_total_errado():
+    assert calcular_total(2, 5) == 11
+
+
+def test_validar_pedido_valido():
+    assert validar_pedido("Coxinha", 2, 5) == "Pedido válido"
+
+
+def test_validar_pedido_item_vazio():
+    assert validar_pedido("", 2, 5) == "Pedido inválido"
+
+
+def test_validar_pedido_quantidade_invalida():
+    assert validar_pedido("Pastel", 0, 5) == "Pedido inválido"
