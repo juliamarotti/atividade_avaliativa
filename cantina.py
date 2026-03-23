6dc6fcc40e3d026cbc9ee411dd3eaffbbee108a0
juliamarotti <julealmarotti@gmail.com>
2026-03-23 17:13:06 -0300

    modificação função

--- cantina.py
+++ cantina.py
@@ -2,7 +2,7 @@ def calcular_total(quantidade, valor_unitario):
     return quantidade * valor_unitario
 
 def validar_pedido(item, quantidade, valor_unitario):
-     if item == "" or quantidade > 0 or valor_unitario > 0:
+     if item and quantidade > 0 and valor_unitario > 0:
         return "Pedido válido"
      else:
         return "Pedido Inválido"
